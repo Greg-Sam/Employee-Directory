59a90197875a58981771c342127b3a35b3f55dd8
Greg Samuelson <grssam@gmail.com>
2021-01-04 15:46:57 -0800

    added filters

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import ReactTable from 'react-table-v6'
 import './App.css';
 import 'react-table-v6/react-table.css'
 import people from './people.js'
+import { render } from '@testing-library/react';
 
 const App = () => {
   
   const columns = [
     {
       Header: 'First Name',
-      accessor: 'first_name'
+      accessor: 'first_name',
+      filterable: true,
     },
     {
       Header: 'Last Name',
-      accessor: 'last_name'
+      accessor: 'last_name',
+      filterable: true,
     },
     {
       Header: 'Email',
@@ -23,7 +26,6 @@ const App = () => {
       accessor: 'title'
     },
   ]
-  
   return (
     <ReactTable
     data = {people}
